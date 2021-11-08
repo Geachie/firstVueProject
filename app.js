a29b150cc37210aef393d791751db7b653593445
Paul McGeachie <paulmcgeachie@icloud.com>
2021-11-08 21:51:39 +0000

    converted to JS to Vue

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-"use strict";
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
 
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-const addGoal = () => {
-  const enteredValue = inputEl.value;
-  const listItemEL = document.createElement("li");
-  listItemEL.textContent = enteredValue;
-  listEl.appendChild(listItemEL);
-  inputEl.value = "";
-};
+// const addGoal = () => {
+//   const enteredValue = inputEl.value;
+//   const listItemEL = document.createElement("li");
+//   listItemEL.textContent = enteredValue;
+//   listEl.appendChild(listItemEL);
+//   inputEl.value = "";
+// };
 
-buttonEl.addEventListener("click", addGoal);
+// buttonEl.addEventListener("click", addGoal);
